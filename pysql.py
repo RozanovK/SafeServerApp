27a2fa07e89843e7cb5c514e14be6f6de946dea3
RozanovKa <rozanuch@gmail.com>
2017-05-23 13:59:09 +0200

    zmodyfikowany: pysql.py 	zmodyfikowany: .idea/workspace.xml

--- pysql.py
+++ pysql.py
@@ -2,23 +2,16 @@ import pymysql
 import hashlib
 
 
-
 def checkAuth(login, passwd):
-        db = pymysql.connect("localhost","rozanovk","siema","login")
-        cursor = db.cursor()
-        cursor.execute('SELECT password FROM users WHERE login = %s', login)
-        results = str(cursor.fetchone()[0])
-        db.close()
-        password = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
-        if results == password:
-            return True
-        else:
-            return False
-
-
-if __name__ == "__main__":
-    if checkAuth('rozanovk', 'siema'):
-        print('Autoryzacja poszła pomyślnie')
+    db = pymysql.connect("localhost", "rozanovk", "siema", "login")
+    cursor = db.cursor()
+    pwd = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
+    cursor.execute('SELECT password FROM users WHERE login = "%s" AND password = "%s"' % (login, pwd))
+    results = str(cursor.fetchone()[0])
+    db.close()
+    if results == pwd:
+        return True
     else:
-        print('Złe hasło')
+        return False
+
 
