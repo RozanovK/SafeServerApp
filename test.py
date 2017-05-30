f3394acae1b7c6bc382b2f07aff26b7383eac5d0
RozanovKa <rozanuch@gmail.com>
2017-05-30 16:22:04 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from pysql import checkAuth
-
-if __name__ == "__main__":
-    if checkAuth('rozanovk', 'siema'):
-        print('Autoryzacja poszła pomyślnie')
-    else:
-        print('Złe hasło')
