b9d3229b21ef6ee6b6aae4838e163451a04887e6
RozanovKa <rozanuch@gmail.com>
2017-06-01 15:28:13 +0200

    zmodyfikowany: snippet.py

--- snippet.py
+++ snippet.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import pymysql
 from pysql import database_connect
 
 
@@ -14,7 +13,7 @@ def get_snippet(headers, body, data, login):
     id = int(cursor.fetchone()[0])
     f = '%Y-%m-%d %H:%M:%S'
     time = datetime.now().strftime(f)
-    filename = login + name + '.txt'
+    filename = '{0}.txt'.format(name)
     cursor.execute('''INSERT INTO snippets(id, datetime, path) VALUES(%s, %s, %s)''' % (id, time, filename))
     db.commit()
     db.close()
@@ -25,8 +24,8 @@ def get_snippet(headers, body, data, login):
 
 
 def put_snippet(login):
-    db = pymysql.connect("localhost", "rozanovk", "siema", "login")
-    cursor = db.cursor()
+    db, cursor = database_connect()
+
 
 
 put_snippet('rozanovk')
