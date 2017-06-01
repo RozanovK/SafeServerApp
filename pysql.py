8134e3baf9b3c0fe31032c5f15dca7a6cc5c57bb
RozanovKa <rozanuch@gmail.com>
2017-06-01 15:24:34 +0200

    zmodyfikowany: pysql.py 	zmodyfikowany: snippet.py

--- pysql.py
+++ pysql.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-
 import socket
 from builtins import str
 import datetime
@@ -7,17 +6,25 @@ import hashlib
 import pymysql
 from vial import render_template
 
+DATABASE_HOST = 'localhost'
+DATABASE_LOGIN = 'rozanovk'
+DATABASE_PASSWORD = 'siema'
+DATABASE_NAME = 'login'
+
+
+def database_connect():
+    db = pymysql.connect(DATABASE_HOST, DATABASE_LOGIN, DATABASE_PASSWORD, DATABASE_NAME)
+    cursor = db.cursor()
+    return db, cursor
 
 
 def get_client_address(environ):
-
     try:
         return environ['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
     except KeyError:
             return environ['REMOTE_ADDR']
 
 def get_own_ip():
-
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         s.connect(('10.255.255.255', 1))
@@ -28,41 +35,36 @@ def get_own_ip():
         s.close()
     return IP
 
-def auth(headers, body, data):
 
+def auth(headers, body, data):
     login = str(data['name']) if 'name' in data else ''
     passwd = str(data['pw']) if 'name' in data else ''
-    if checkAuth(login, passwd):
-        if banIP(login):
-            IP, time = printIP(login)
+    if check_auth(login, passwd):
+        if ban_ip(login):
+            IP, time = print_ip(login)
             return render_template('mainpage.html', body=body, data=data, IP= IP, time=time), 200, {}
 
-def checkAuth(login, passwd):
 
+def check_auth(login, passwd):
     #ip = get_client_address()
     ip = get_own_ip()
     pwd = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
 
-    db = pymysql.connect("localhost", "rozanovk", "siema", "login")
-    cursor = db.cursor()
+    db, cursor = database_connect()
     cursor.execute('SELECT password FROM users WHERE login = "%s"' % login)
     results = str(cursor.fetchone()[0])
     if results == pwd:
-        insertLog(ip, login, "Y")
+        insert_log(ip, login, "Y")
         return True
     else:
-         insertLog(ip, login, "N")
+         insert_log(ip, login, "N")
          return False
 
     db.close()
 
 
-
-
-def banIP(login):
-
-    db = pymysql.connect("localhost", "rozanovk", "siema", "login")
-    cursor = db.cursor()
+def ban_ip(login):
+    db,cursor = database_connect()
     cursor.execute(
         '''SELECT COUNT(*) FROM logs WHERE validation="N" AND login = %s AND (TIMESTAMPDIFF(HOUR, time, Now()) < 1)''',
         login)  # sprawdzamy ilość niepoprawnych walidacji w ciągu ostatniej godziny
@@ -73,22 +75,18 @@ def banIP(login):
         return False
 
 
-
-def insertLog(ip, login, validation):
-
+def insert_log(ip, login, validation):
     f = '%Y-%m-%d %H:%M:%S'
     time = datetime.datetime.now().strftime(f)
 
-    db = pymysql.connect("localhost", "rozanovk", "siema", "login")
-    cursor = db.cursor()
+    db, cursor = database_connect()
     cursor.execute('''INSERT INTO logs (ip, login, time, validation) VALUES (%s, %s, %s, %s)''', (ip, login, time, validation))
     db.commit()
     db.close
 
 
-def printIP(login):
-    db = pymysql.connect("localhost", "rozanovk", "siema", "login")
-    cursor = db.cursor()
+def print_ip(login):
+    db, cursor = database_connect()
     cursor.execute('''SELECT ip, time from logs WHERE login = %s AND validation = "N"''', login)
     #logowania = cursor.fetchall()
     #for i in logowania:
@@ -98,20 +96,17 @@ def printIP(login):
     return IP, datetime
 
 
-
-
-def forgotPasswd(headers, body, data):      #check if login exists in database
+def forgot_password(headers, body, data):      #check if login exists in database
     login = str(data['name']) if 'name' in data else ''
-    answerEntered = str(data['answer']) if 'answer' in data else ''
+    answer_entered = str(data['answer']) if 'answer' in data else ''
 
-    db = pymysql.connect("localhost", "rozanovk", "siema", "login")
-    cursor = db.cursor()
+    db, cursor = database_connect()
     cursor.execute('''SELECT * from users WHERE login = %s ''', login)
 
     if str(cursor.fetchone()[0]) != 0:
         cursor.execute('''SELECT answer FROM users WHERE login = %s''', login)
         answerdb = str(cursor.fetchone()[0])
-        a = hashlib.sha256(answerEntered.encode('utf-8')).hexdigest()
+        a = hashlib.sha256(answer_entered.encode('utf-8')).hexdigest()
         if answerdb == a:
             return render_template('passwordchange.html', body=body, data=data), 200, {}
         else:
@@ -120,16 +115,13 @@ def forgotPasswd(headers, body, data):      #check if login exists in database
         return render_template('wronganswer.html', body=body, data=data), 200, {}
 
 
-def insertNewPass(headers, body, data):
-
+def insert_new_password(headers, body, data):
     passwd = str(data['pw']) if 'pw' in data else ''
     passwd_r = str(data['pw-x']) if 'pw-x' in data else ''
     pw = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
     if passwd == passwd_r:
-        db = pymysql.connect("localhost", "rozanovk", "siema", "login")
-        cursor = db.cursor()
+        db, cursor = database_connect()
         cursor.execute('''UPDATE users SET password= %s WHERE login= %s''' % (pw, login))
-
 
 
 
--- snippet.py
+++ snippet.py
@@ -1,45 +1,35 @@
 from datetime import datetime
-import pathlib
+import pymysql
+from pysql import database_connect
 
 
+def get_snippet(headers, body, data, login):
 
-def getSnippet(headers, body, data, login):
-
+    db, cursor = database_connect()
     snippet = str(data['snippet']) if 'snippet' in data else ''
+    name = str(data['name']) if 'name' in data else ''
 
+    name = name.replace('\t', '_')
+    cursor.execute('''SELECT id FROM users WHERE login=%s''', login)
+    id = int(cursor.fetchone()[0])
     f = '%Y-%m-%d %H:%M:%S'
     time = datetime.now().strftime(f)
-    filename = login + '.txt'
+    filename = login + name + '.txt'
+    cursor.execute('''INSERT INTO snippets(id, datetime, path) VALUES(%s, %s, %s)''' % (id, time, filename))
+    db.commit()
+    db.close()
+
     file = open(filename, 'w')
-    file.write(time + '\n')
-    file.write(snippet + '\n')
+    file.write(name)
+    file.write(snippet)
 
 
-def putSnippet(login):
-
-    snippets = []
-    datet = []
-    def validate(d):
-        try:
-            datetime.strptime(d, "%Y-%m-%d %H:%M:%S\n")
-            return True
-        except ValueError:
-            return False
-
-    filename = login + '.txt'
-    path = pathlib.Path(filename)
-    if path.is_file():
-        with open(filename, 'r') as f:
-            for line in f:
-                if validate(line):
-                    datet.append(line)
-                    print(datet[0])
-                else:
-                    snippets.append(line)
-        print (snippets)
+def put_snippet(login):
+    db = pymysql.connect("localhost", "rozanovk", "siema", "login")
+    cursor = db.cursor()
 
 
-putSnippet('rozanovk')
+put_snippet('rozanovk')
 
 
 
